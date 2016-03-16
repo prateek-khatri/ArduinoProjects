18fdaf51bfb2a09ff4703afde1caec0a24f3117b
Prateek Khatri <prateek.khatri90@gmail.com>
2016-03-16 15:59:24 -0700

    Create ESPfinal.ino

---
+++ HydroponicsIOT/ESPfinal.ino
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+ESP8266WiFiMulti WiFiMulti;
+
+//GLOBAL DEFINITIONS
+#define SERIAL_RX 2
+#define SERIAL_TX 3
+
+//SENSOR VALUES
+int soil_moisture;
+int light_intensity;
+float pHValue;
+float ecValue;
+
+//THRESHOLDS
+int soil_moisture_min;
+int light_intensity_min;
+float pH_min;
+float pH_max;
+float ec_min;
+float ec_max;
+
+#include <SoftwareSerial.h>
+SoftwareSerial ESPserial(2, 3); // RX | TX
+
+void setup() {
+
+
+
+}
+
+void loop() {
+    
+}
