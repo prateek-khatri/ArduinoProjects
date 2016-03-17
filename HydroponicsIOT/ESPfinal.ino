566ff16f042b841a82e32cd12f66513426207134
Prateek Khatri <prateek.khatri90@gmail.com>
2016-03-16 23:26:27 -0700

    Update ESPfinal.ino

--- HydroponicsIOT/ESPfinal.ino
+++ HydroponicsIOT/ESPfinal.ino
@@ -1,3 +1,4 @@
+#include <ArduinoJson.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
@@ -35,6 +36,30 @@ String payload;
 #include <SoftwareSerial.h>
 SoftwareSerial ESPserial(SERIAL_RX, SERIAL_TX); // RX | TX
 
+void updatePayloadOnArduino()
+{
+  ESPserial.flush();
+  //{"tdsUpperThresholds":0,"lightLowerThresholds":0,"tdsLowerThresholds":0,"moistureLowerThresholds":0,"phLowerThresholds":0,"phUpperThresholds":0}
+  StaticJsonBuffer<200> jsonBuffer;
+  char payloadArray[200]; 
+  payload.toCharArray(payloadArray,200);
+  JsonObject& root = jsonBuffer.parseObject(payloadArray);
+//  if(!root.success())
+//  {
+//    Serial.println("Json Conversion Failed"); 
+//    return;
+//  }
+  soil_moisture_min = root["moistureLowerThresholds"];
+  light_intensity_min = root["lightLowerThresholds"];
+  pH_min = root["phUpperThresholds"];
+  pH_max = root["phLowerThresholds"];
+  ec_min = root["tdsLowerThresholds"];
+  ec_max = root["tdsUpperThresholds"];
+  String responseThresholds = ""+String(pH_min)+","+String(pH_max)+","+String(ec_min)+","+String(ec_max)+","+String(soil_moisture_min)+","+String(light_intensity_min);
+  ESPserial.print(responseThresholds);
+  ESPserial.flush();
+}
+
 void sendInitToServer()
 {
     bool flag = false;
@@ -50,7 +75,7 @@ void sendInitToServer()
         Serial.print("[HTTP] begin...\n");
 
         //configure server request
-        http.begin("172.20.10.1", 8080,"?&fn=getSensorDetails&sensorId=esp001&ph=7.2&tds=50&light=15.3&moisture=15.99&boot=false");
+        http.begin("172.20.10.1", 8080,"?&fn=getSensorDetails&sensorId=esp001&ph=7.2&tds=50&light=15.3&moisture=15.99&boot=true");
         Serial.println("[HTTP] GET...");
 
         //send HTTP header
@@ -64,13 +89,19 @@ void sendInitToServer()
           if(httpCode == HTTP_CODE_OK)
           {
             payload = http.getString();
+            //payload.replace("\n","");
+            //payload.replace(" ","");
             Serial.println("Received From Server:");
             Serial.println(payload);
-            
-            flag = true;
+                       
             if(payload.equals(""))
             {
               flag=false;
+            }
+            else
+            {
+              flag = true;
+              break;
             }
           }
         }
@@ -118,9 +149,54 @@ void setup()
     }
     delay(500);
    }
+   updatePayloadOnArduino();
+}
+/*
+ * Code 0: Normal Delta Update
+ * Code 2: Threshold Violation, need to follow with actuator status
+ * Code 9: Actuator status string
+ */
+
+void waitForSensorUpdates()
+{
+  while(!(ESPserial.available() >0))
+  {
+    delay(1000);
+  }
+  String response = "";
+  while(ESPserial.available() >0)
+  {
+    ESPserial.read();
+    if(a == '0' || a == '2' || a=='9')
+    {
+      ESPserial.read();
+      break;
+    }
+  }
+  payload = "";
+  while(ESPserial.available() >0)
+  {
+   char b = ESPserial.read();
+    payload +=b;
+  }
+  if(a=='0')
+  {
+    deltaViolationUpdate(payload);
+  }
+  if(a=='2')
+  {
+    thresholdViolationUpdate(payload);
+  }
+  if(a=='9')
+  {
+    actuatorStatusUpdate(payload);
+  }
+
+  
 }
 
 void loop() 
 {
-    
+    waitForSensorUpdates();
+    delay(1000);
 }
