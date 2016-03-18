bfb3e375a34dc5374c8d386309fcf66a37322c63
Prateek Khatri <prateek.khatri90@gmail.com>
2016-03-18 04:06:34 -0700

    Final Code, pending test with server

--- NewMETHOD/ESPMaster.ino
+++ NewMETHOD/ESPMaster.ino
@@ -31,6 +31,11 @@ float pH_max;
 float ec_min;
 float ec_max;
 
+//DELTAS
+float ph_delta = 0.25;
+float ec_delta = 10;
+int light_delta = 3;
+int moisture_delta = 5;
 
 //Actuator Status
 bool phActuator;
@@ -85,12 +90,19 @@ void initPeripherals()
 //**********************INIT FUNCTIONS END************************************
 
 //**********************BOOT TIME REQUEST THRESHOLDS**************************
-void updateThresholds() //THIS FUNCTION CAN BE CALLED WHEN PAYLOAD STRING HAS JSON OBJECT WITH THRESHOLDS
+boolean updateThresholds() //THIS FUNCTION CAN BE CALLED WHEN PAYLOAD STRING HAS JSON OBJECT WITH THRESHOLDS
 {
+
   StaticJsonBuffer<200> jsonBuffer;
   char payloadArray[200]; 
   payload.toCharArray(payloadArray,200);
   JsonObject& root = jsonBuffer.parseObject(payloadArray);
+  int valid = root["valid"];
+  if(valid == 0)
+  {
+    Serial.println("Invalid Thresholds...wait for Server...");
+    delay(2000);
+  }
   soil_moisture_min = root["moistureLowerThresholds"];
   light_intensity_min = root["lightLowerThresholds"];
   pH_min = root["phUpperThresholds"];
@@ -100,10 +112,12 @@ void updateThresholds() //THIS FUNCTION CAN BE CALLED WHEN PAYLOAD STRING HAS JS
   String responseThresholds = ""+String(pH_min)+","+String(pH_max)+","+String(ec_min)+","+String(ec_max)+","+String(soil_moisture_min)+","+String(light_intensity_min);
   Serial.println("Thresholds Received :");
   Serial.println(responseThresholds);
+  return true;
 }
-void requestInitThresholds()
+boolean requestInitThresholds()
 {
     bool flag = false;
+   
     Serial.println("Fethching Thresholds from server");
     Serial.println("Waiting for WiFi connection...");
     while(flag == false)
@@ -112,7 +126,7 @@ void requestInitThresholds()
       if((WiFiMulti.run() == WL_CONNECTED)) 
       {
         Serial.println("WiFi Connection Established!");
-        HTTPClient http;
+         HTTPClient http;
         Serial.print("[HTTP] begin...\n");
 
         //configure server request
@@ -132,30 +146,23 @@ void requestInitThresholds()
             payload = http.getString();
             Serial.println("Received From Server:");
             Serial.println(payload);
-                       
-            if(payload.equals(""))
-            {
-              flag=false;
-            }
-            else
-            {
-              flag = true;
-              break;
-            }
-          }
-        }
-        else
-        {
-          Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-        http.end();
-        
-      }
-      delay(10000);
+            flag = true;
+           }
+         }
+       
+       else
+       {
+         Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+       }
+      
+       http.end();
+       delay(2000); 
+     }
     }
-
-    updateThresholds();
+   flag = updateThresholds();
+   return flag;
   
+
 }
 //**********************BOOT TIME REQUEST THRESHOLDS END**************************
 
@@ -191,7 +198,7 @@ void parseSensorValues(String vals)
 }
 void readSensorValues()
 {
-  //ESPSerial.flush();
+  ESPSerial.flush();
   Serial.println("Waiting for Values from Arduino...");
   while(!(ESPSerial.available() >0))
   {
@@ -219,15 +226,338 @@ void readSensorValues()
   parseSensorValues(sensorRead);
 }
 //**********************SENSOR READING END********************************
+
+
+//**********************SENDING UPDATES TO SERVER*************************
+boolean sendUpdate()
+{
+   bool flag = false;
+   
+    Serial.println("Sending Values to Server...");
+    Serial.println("Waiting for WiFi connection...");
+    while(flag == false)
+    {
+      Serial.println("Trying to connect...");
+      if((WiFiMulti.run() == WL_CONNECTED)) 
+      {
+        Serial.println("WiFi Connection Established!");
+         HTTPClient http;
+        Serial.print("[HTTP] begin...\n");
+
+        //configure server request
+        http.begin("172.20.10.1", 8080,"?&fn=getSensorDetails&sensorId=esp001&ph="+String(pHValue)+"&tds="+String(ecValue)+"&light="+String(light_intensity)+"&moisture="+String(soil_moisture)+"&boot=false");
+        Serial.println("[HTTP] GET...");
+
+        //send HTTP header
+        int httpCode = http.GET();
+
+        //httpCode will be negative on error
+        if(httpCode >0)
+        {
+          Serial.print("[HTTP] GET... code ");Serial.println(httpCode);
+
+          if(httpCode == HTTP_CODE_OK)
+          {
+            payload = http.getString();
+            Serial.println("Received From Server:");
+            Serial.println(payload);
+            flag = true;
+           }
+         }
+       
+       else
+       {
+         Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+       }
+      
+       http.end();
+       
+     }
+     delay(2000); 
+    }
+   flag = updateThresholds();
+   return flag;
+  
+}
+boolean sendActuatorUpdate(bool ph,bool ec,bool light,bool moisture)
+{
+   bool flag = false;
+   
+    Serial.println("Sending Actuator Update to server");
+    Serial.println("Waiting for WiFi connection...");
+    while(flag == false)
+    {
+      Serial.println("Trying to connect...");
+      if((WiFiMulti.run() == WL_CONNECTED)) 
+      {
+        Serial.println("WiFi Connection Established!");
+         HTTPClient http;
+        Serial.print("[HTTP] begin...\n");
+
+        //configure server request
+        http.begin("172.20.10.1", 8080,"?&fn=updateActuatorData&sensorId=esp001&ph="+String(ph)+"&tds="+String(ec)+"&light="+String(light)+"&moisture="+String(moisture)+"&boot=false");
+        Serial.println("[HTTP] GET...");
+
+        //send HTTP header
+        int httpCode = http.GET();
+
+        //httpCode will be negative on error
+        if(httpCode >0)
+        {
+          Serial.print("[HTTP] GET... code ");Serial.println(httpCode);
+
+          if(httpCode == HTTP_CODE_OK)
+          {
+            payload = http.getString();
+            Serial.println("Received From Server:");
+            Serial.println(payload);
+            flag = true;
+           }
+         }
+       
+       else
+       {
+         Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+       }
+      
+       http.end();
+       
+     }
+     delay(2000); 
+    }
+   flag = updateThresholds();
+   return flag;
+  
+}
+
+//**********************SENDING UPDATES TO SERVER END*********************
+
+//**********************RELAY CONTROLS************************************
+void activate_ph_pumps()
+{
+  if(pHValue < pH_min)
+  {
+    //activate base
+    digitalWrite(RELAY_PH_BASE_PUMP,HIGH);
+    digitalWrite(RELAY_PH_ACID_PUMP,LOW);
+  }
+  else if(pHValue > pH_max)
+  {
+    //activate acid
+    digitalWrite(RELAY_PH_BASE_PUMP,LOW);
+    digitalWrite(RELAY_PH_ACID_PUMP,HIGH);
+  }
+  phActuator = true;
+}
+void deactivate_ph_pumps()
+{
+  digitalWrite(RELAY_PH_BASE_PUMP,LOW);
+  digitalWrite(RELAY_PH_ACID_PUMP,LOW);
+  phActuator = false;
+}
+
+void activate_ec_pumps()
+{
+  if(ecValue < ec_min)
+  {
+    //activate base
+    digitalWrite(RELAY_EC_PUMP_ONE,HIGH);
+    digitalWrite(RELAY_EC_PUMP_TWO,LOW);
+  }
+  else if(ecValue > ec_max)
+  {
+    //activate acid
+    digitalWrite(RELAY_EC_PUMP_ONE,LOW);
+    digitalWrite(RELAY_EC_PUMP_TWO,HIGH);
+  }
+  ecActuator = true;
+}
+void deactivate_ec_pumps()
+{
+    digitalWrite(RELAY_EC_PUMP_ONE,LOW);
+    digitalWrite(RELAY_EC_PUMP_TWO,HIGH);
+    ecActuator = false;
+}
+
+void activate_light_switch()
+{
+  digitalWrite(RELAY_LIGHT_SWITCH,HIGH);
+  lightActuator = true;
+}
+void deactivate_light_switch()
+{
+  digitalWrite(RELAY_LIGHT_SWITCH,LOW);
+  lightActuator = false;
+}
+
+void activate_water_pump()
+{
+  digitalWrite(RELAY_WATER_PUMP,HIGH);
+  waterActuator = true;
+}
+void deactivate_water_pump()
+{
+  digitalWrite(RELAY_WATER_PUMP,LOW);
+  waterActuator = false;
+}
+//**********************RELAY CONTROLS END********************************
+
+//**********************MATCHING THRESHOLDS/DELTAS************************
+void matchDeltas()
+{
+  float prev_ph = pHValue;
+  float prev_ec = ecValue;
+  int prev_light = light_intensity;
+  int prev_moisture = soil_moisture;
+
+  readSensorValues();
+  if( (abs(prev_ph - pHValue) > ph_delta) || (abs(prev_ec - ecValue) > ec_delta) || (abs(prev_light - light_intensity) > light_delta) || (abs(prev_moisture - soil_moisture)>moisture_delta))
+  {
+    while(!sendUpdate())
+   {
+    delay(500);
+   }
+  }
+}
+
+void matchThresholds(bool ph,bool ec,bool light,bool moisture)
+{
+  //Send Updates Acquires Sensor Values, Sends to server and updates thresholds
+  while(!sendUpdate())
+  {
+    delay(500);
+  }
+  while(ph || ec || light || moisture)
+  {
+    /*
+     * Activate: activate relevant actuator
+     * Deactivate: deactivate all actuators relevant
+     * both update actuator status
+     */
+    if(pHValue < pH_min || pHValue > pH_max)
+    {
+      ph = true;
+      activate_ph_pumps();
+    }
+    else
+    {
+      ph =  false;
+      deactivate_ph_pumps();
+    }
+  
+    if(ecValue < ec_min || ecValue > ec_max)
+    {
+      ec = true;
+      activate_ec_pumps();
+    }
+    else
+    {
+      ec = false;
+      deactivate_ec_pumps();
+    }
+  
+    if(light_intensity < light_intensity_min)
+    {
+      light = true;
+      activate_light_switch();
+    }
+    else
+    {
+      light = false;
+      deactivate_light_switch();
+    }
+  
+    if(soil_moisture < soil_moisture_min)
+    {
+      moisture = true;
+      activate_water_pump();
+    }
+    else
+    {
+      moisture = false;
+      deactivate_water_pump();
+    }
+    while(!sendActuatorUpdate(ph,ec,light,moisture))
+    {
+      delay(500);
+    }
+    matchDeltas();
+  }
+}
+void matchValues()
+{
+  bool pHViolation = false;
+  bool ecViolation = false;
+  bool lightViolation = false;
+  bool moistureViolation = false;
+  if(pHValue < pH_min || pHValue > pH_max)
+  {
+    pHViolation = true;
+  }
+  else
+  {
+    pHViolation = false;
+  }
+  if(ecValue < ec_min || ecValue > ec_max)
+  {
+    ecViolation = true;
+  }
+  else
+  {
+    ecViolation = false;
+  }
+  if(light_intensity < light_intensity_min)
+  {
+    lightViolation = true;
+  }
+  else
+  {
+    lightViolation = false;
+  }
+  if(soil_moisture < soil_moisture_min)
+  {
+    moistureViolation = true;
+  }
+  else 
+  {
+    moistureViolation = false;
+  }
+
+  //CALL THRESHOLD MATCHING OR DELTA MATCHING
+  if(pHViolation == true || ecViolation == true || lightViolation == true || moistureViolation == true)
+  {
+    matchThresholds(pHViolation, ecViolation,lightViolation,moistureViolation);
+  }
+  else
+  {
+    matchDeltas();
+  }
+}
+//**********************MATCHING THRESHOLDS/DELTAS END********************
+
 void setup() 
 {
   initPeripherals();
-  //requestInitThresholds();
-   
-  
+  while(!requestInitThresholds());
+  //don't go forwad until valid thresholds are received.  
 }
 
 void loop() 
 {
-  readSensorValues(); 
+  readSensorValues();
+  /*
+   * Match Thresholds, if true then
+   * 1. send update to server - update thresholds
+   * 2. activate respective actuators
+   * 3. send actuator status - update thresholds
+   * 4. readSensorValues until inside thresholds - send update at deltas
+   * 5. deactivate actuators
+   * 6. send actuator status - update thresholds
+   * 
+   * Match Deltas, if true then
+   * 1. read sensor values
+   * 2. send update
+   * 3. update thresholds
+   */
+  matchValues();
 }
